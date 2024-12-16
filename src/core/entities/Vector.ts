2302756b32111a4ba651db00f3dc7685e7c19ece
fendros <tristan.schmitt@epitech.eu>
2024-12-16 13:18:24 +0100

    feat(core): add rotation & length methods

--- src/core/entities/Vector.ts
+++ src/core/entities/Vector.ts
@@ -5,6 +5,10 @@ export class Vector extends Point {
     super(x, y, z);
   }
 
+  public get length() {
+    return Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z);
+  }
+
   public add(vector: Vector) {
     this._x += vector.x;
     this._y += vector.y;
@@ -39,5 +43,32 @@ export class Vector extends Point {
     this._z /= scalar;
 
     return this;
+  }
+
+  public rotateXY(angle: number) {
+    this.rotate('_x', '_y', angle);
+  }
+
+  public rotateXZ(angle: number) {
+    this.rotate('_x', '_z', angle);
+  }
+
+  public rotateYZ(angle: number) {
+    this.rotate('_y', '_z', angle);
+  }
+
+  private rotate(
+    axis1: '_x' | '_y' | '_z',
+    axis2: '_x' | '_y' | '_z',
+    angle: number,
+  ) {
+    const cos = Math.cos(angle);
+    const sin = Math.sin(angle);
+
+    const a = this[axis1];
+    const b = this[axis2];
+
+    this[axis1] = cos * a - sin * b;
+    this[axis2] = sin * a + cos * b;
   }
 }
